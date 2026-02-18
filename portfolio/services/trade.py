40fc8e43590dc21b374dbba2adadcabf4c630edd
ivan <ivan.s.rivera@gmail.com>
2026-02-18 21:07:16 +1100

    add alpaca

--- portfolio/services/trade.py
+++ portfolio/services/trade.py
@@ -3,6 +3,9 @@
 import logging
 from functools import lru_cache
 
+from alpaca.data.historical.stock import StockHistoricalDataClient
+from alpaca.data.requests import StockLatestTradeRequest
+
 from portfolio.config.settings import Settings, get_settings
 
 logger = logging.getLogger(__name__)
@@ -15,8 +18,17 @@ class TradeService:
         self._settings = settings
 
     def run(self) -> None:
-        """Execute the trade flow. Stub implementation logs hello world."""
-        logger.info("Hello, world! Trade background job started.")
+        """Execute the trade flow by fetching and printing the latest MSFT price."""
+        # TODO: clean this up! This is just a test to see if we can call the API
+        client = StockHistoricalDataClient(
+            api_key=self._settings.alpaca_api_key,
+            secret_key=self._settings.alpaca_secret_key,
+        )
+        request = StockLatestTradeRequest(symbol_or_symbols="MSFT")
+        latest_trades = client.get_stock_latest_trade(request)
+        msft_trade = latest_trades["MSFT"]
+
+        logger.info("MSFT latest trade price: %s", msft_trade.price)
 
     def resume(self) -> None:
         """Resume a trade after receiving human approval or rejection.
